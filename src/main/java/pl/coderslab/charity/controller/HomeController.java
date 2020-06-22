e45494bb5500b4588d609a592824ced4435e30dc
Piotr Grabowski <grabowskipiotr1989@gmail.com>
2020-06-22 20:21:01 +0200

    Suma przekazanych datkow oraz ilosc workow

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import java.util.List;
@@ -14,14 +15,26 @@ import java.util.List;
 public class HomeController {
 
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @ModelAttribute("institutions")
     public List<Institution> institutions() {
         return institutionRepository.findAll();
+    }
+
+    @ModelAttribute("bagsQuantity")
+    public Integer bagsQuantity() {
+        return donationRepository.sumOfBags();
+    }
+
+    @ModelAttribute("donationsQuantity")
+    public Integer donationsQuantity() {
+        return donationRepository.findAll().size();
     }
 
     @RequestMapping("/")
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -22,7 +22,7 @@
 <section class="stats" id="stats">
     <div class="container container--85">
         <div class="stats--item">
-            <em>13</em>
+            <em>${bagsQuantity}</em>
 
             <h3>Oddanych worków</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est beatae, quod accusamus illum
@@ -30,7 +30,7 @@
         </div>
 
         <div class="stats--item">
-            <em>5</em>
+            <em>${donationsQuantity}</em>
             <h3>Przekazanych darów</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam magnam, sint nihil cupiditate quas
                 quam.</p>
